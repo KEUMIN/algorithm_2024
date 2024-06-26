ea0fbdadc9f5f0d786b68c0e445d93450a82ce45
keumminseop <mskeum93@naver.com>
2024-06-26 20:37:53 +0900

    프로그래머스 131130 문제풀이

--- tests/programmers_135808.py
+++ tests/programmers_135808.py
@@ -8,4 +8,7 @@ def solution(k, m, score):
     
     return answer
 
+def other_solution(k, m, score):
+    return sum(sorted(score)[len(score)%m::m])*m
+
 print(solution(4,3,[4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2]))
