cd299d265d6c7023a3e9f3dfa893ac690884282a
keumminseop <mskeum93@naver.com>
2024-09-26 22:46:07 +0900

    240926 [PCCP 기출문제] 4번 / 수식 복원하기 - 수정

--- tests/level3/restore_expressions.py
+++ tests/level3/restore_expressions.py
@@ -1,58 +1,59 @@
 # 340210
 # 내 풀이
 def solution(expressions):
-    unsolved_exp = []
-    min_base = 2
-    for exp in expressions:
-        comps = exp.split(" ")
-        for i, comp in enumerate(comps):
-            if i == 0 or i == 2:
-                min_base = max(min_base, int(comp) % 10)
-            if i == 4 and comp == "X":
-                unsolved_exp.append(exp)
-    possible_base = set()
-    for i in range(min_base, 10):
-        possible = False
-        for exp in expressions:
-            if exp[len(exp) - 1] == "X":
-                continue
-            comps = exp.split(" ")
-            a = parse_to_base(n=i, target=int(comps[0]))
-            b = parse_to_base(n=i, target=int(comps[2]))
-            c = parse_to_base(n=i, target=int(comps[4]))
-            if comps[1] == "+":
-                if a + b == c:
-                    possible = True
-                else:
-                    possible = False
-            elif comps[1] == "-":
-                if a - b == c:
-                    possible = True
-                else:
-                    possible = False
-        if possible:
-            possible_base.add(i)
-    result = []
-    for exp in unsolved_exp:
-        comps = exp.split(" ")
-        possibles = set()
-        for i in possible_base:
-            a = parse_to_base(n=i, target=int(comps[0]))
-            b = parse_to_base(n=i, target=int(comps[2]))
-            if comps[1] == "+":
-                possibles.add(a + b)
-            elif comps[1] == "-":
-                possibles.add(a - b)
-        if len(possibles) > 1:
-            result.append(exp.replace("X", "?"))
-        elif len(possibles) == 1:
-            result.append(exp.replace("X", str(possibles.pop())))
-    return result
-def parse_to_base(n, target):
-    if target >= 100:
-        return (target // 100) * (n ** 2) + ((target % 100) // 10) * n + (target % 10)
-    elif 10 <= target < 100:
-        return (target // 10) * n + (target % 10)
-    else:
-        return target
-print(solution(expressions=["10 - 2 = X", "30 + 31 = 101", "3 + 3 = X", "33 + 33 = X"]))
+    def decimal_to_base(n, base):
+        if n == 0:
+            return 0
+        result = []
+        while n:
+            result.append(str(n % base))
+            n //= base
+        return int(''.join(result[::-1]))
+
+    def calculate_X(expression:str, base: int):
+        exp = expression.split()
+        a, b, op = int(exp[0], base), int(exp[2], base), exp[1]
+        return decimal_to_base(a+b, base) if op == '+' else decimal_to_base(a-b, base)
+
+    def valid_base_expression(expression: str, base: int):
+        exp = expression.split()
+        a, b, c = int(exp[0], base), int(exp[2], base), int(exp[4], base)
+        return (a + b == c) if exp[1] == '+' else (a - b == c)
+
+    def find_base(expression: str):
+        base = [i for i in range(2, 10)]
+        exp = expression.split()
+        min_base = 2
+        for comp in [exp[0], exp[2], exp[4]]:
+            if comp != 'X':
+                for i in range(len(comp)):
+                    min_base = max(min_base, int(comp[i]) + 1)
+        base = list(filter(lambda x: x >= min_base, base))
+
+        result = base[:]
+        if exp[4] != 'X':
+            for num in base:
+                if not valid_base_expression(expression, num):
+                    result.remove(num)
+
+        return result
+
+    candidates = set([i for i in range(2, 10)])
+    unknown = []
+    for expression in expressions:
+        exp = expression.split()
+        tmp_base = find_base(expression)
+        candidates.intersection_update(tmp_base)
+        if exp[4] == 'X':
+            unknown.append(expression)
+
+    answer = []
+    for expression in unknown:
+        temp_val = set()
+        for base in candidates:
+            temp_val.add(calculate_X(expression, base))
+        answer.append(expression[:-1] + (str(temp_val.pop()) if len(temp_val) == 1 else '?'))
+
+    return answer
+
+print(solution(expressions=["2 - 1 = 1", "2 + 2 = X", "7 + 4 = X", "5 - 5 = X"]))
