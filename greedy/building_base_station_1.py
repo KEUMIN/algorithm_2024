84c81e1279dd491bc1d9c14df25801ade495d6d2
keumminseop <mskeum93@naver.com>
2024-10-12 23:10:52 +0900

    241012 그리디 - 기지국 설치 (복습 1회차)

--- greedy/building_base_station_1.py
+++ greedy/building_base_station_1.py
@@ -1,12 +1,13 @@
-#12979
+# 프로그래머스 - 기지국 설치 (LV3) : https://school.programmers.co.kr/learn/courses/30/lessons/12979
+# 사용 알고리즘 : 그리디
+
+
 def solution(N, stations, W):
     answer = 0
     for i in range(len(stations) + 1):
         start = stations[i - 1] + W if i != 0 else 0
-        end = stations[i] - 1 - W if i != len(stations) else N
-
+        end = stations[i] - W - 1 if i != len(stations) else N
         answer += (end - start) // (2 * W + 1)
         if (end - start) % (2 * W + 1) > 0:
             answer += 1
-            
     return answer
