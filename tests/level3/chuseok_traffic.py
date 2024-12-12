ff685b8a61451bc0ef2f6b806ee1d47473193152
keumminseop <mskeum93@naver.com>
2024-12-12 21:23:31 +0900

    241212 브루트 포스 - 추석 트래픽

--- tests/level3/chuseok_traffic.py
+++ tests/level3/chuseok_traffic.py
@@ -1,48 +1,29 @@
 # 프로그래머스 - 추석 트래픽 (LV3) : https://school.programmers.co.kr/learn/courses/30/lessons/17676
-# 사용 알고리즘 : 브루트 포스...
+# 사용 알고리즘 : 브루트 포스
+# 인사이트 : 끝 구간 + 1초 아이디어는 맞았으나, 구간의 시작과 끝 모두 1초 구간을 구하고 N번 다시 순회할거라는 생각은 못했다...
 
 from datetime import datetime, timedelta
 
 
 def solution(lines):
-    cur_end = datetime.now()
-    alpha = []
-    max_cnt = -1
+    # 시작시간과 종료시간을 저장
+    times = []
+    for line in lines:
+        date, end_time, duration = line.split()
+        end = datetime.strptime(end_time, "%H:%M:%S.%f")
+        duration = float(duration[:-1])
+        start = end - timedelta(seconds=duration) + timedelta(milliseconds=1)
+        times.append((start, end))
 
-    for i, line in enumerate(lines):
-        date, end, time = line.split()
-        end = datetime.strptime(end, "%H:%M:%S.%f")
-        time = str(float(time[:-1]))
-        time = datetime.strptime(time, "%S.%f")
-        start = end - time + datetime.strptime("0.001", "%S.%f")
+    max_traffic = 0
+    # 각 로그의 시작과 끝 시점에서 1초 구간 체크
+    for i in range(len(times)):
+        for check_time in (times[i][0], times[i][1]):
+            count = 0
+            check_end = check_time + timedelta(seconds=1)
+            for start, end in times:
+                if start < check_end and end >= check_time:
+                    count += 1
+            max_traffic = max(max_traffic, count)
 
-        if i == 0 or start >= cur_end:
-            cur_end = end + timedelta(seconds=1)
-            alpha.append(end)
-            for et in alpha:
-                if et <= start:
-                    alpha.remove(et)
-            max_cnt = max(max_cnt, len(alpha))
-        else:
-            alpha.append(end)
-            max_cnt = max(max_cnt, len(alpha))
-
-    return max_cnt
-
-
-print(
-    solution(
-        [
-            "2016-09-15 20:59:57.421 0.351s",
-            "2016-09-15 20:59:58.233 1.181s",
-            "2016-09-15 20:59:58.299 0.8s",
-            "2016-09-15 20:59:58.688 1.041s",
-            "2016-09-15 20:59:59.591 1.412s",
-            "2016-09-15 21:00:00.464 1.466s",
-            "2016-09-15 21:00:00.741 1.581s",
-            "2016-09-15 21:00:00.748 2.31s",
-            "2016-09-15 21:00:00.966 0.381s",
-            "2016-09-15 21:00:02.066 2.62s",
-        ]
-    )
-)
+    return max_traffic
