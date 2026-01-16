d549910754f510b3baf109807024b185de6bc98f
keumminseop <mskeum93@naver.com>
2026-01-16 20:53:56 +0900

    LV3 선입 선출 스케줄링

---
+++ R2/12920.py
@@ -0,0 +1,21 @@
+def solution(n, cores):
+    m = len(cores)
+
+    def processed(time):
+        return m + sum([time // c for c in cores])
+
+    left, right = 1, n * max(cores)
+
+    while left <= right:
+        mid = (left + right) // 2
+        if processed(mid) >= n:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    remain = n - processed(left - 1)
+    for i, c in enumerate(cores):
+        if left % c == 0:
+            remain -= 1
+            if remain == 0:
+                return 1 + i
