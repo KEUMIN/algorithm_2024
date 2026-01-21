1c85aa790680e716fc17cfe1852015e7c0693542
KEUMIN <mskeum93@naver.com>
2026-01-21 13:14:41 +0900

    LV3 금과 은 운반하기

--- R1/86053.py
+++ R1/86053.py
@@ -1,11 +1,11 @@
 def solution(a, b, g, s, w, t):
 
-    def can_deliver_in_time(mid):
+    def can_deliver_in_time(time):
         total_gold, total_silver, total = 0, 0, 0
 
         for i in range(len(g)):
-            trips = mid // (2 * t[i])  # 왕복 횟수
-            if mid % (2 * t[i]) >= t[i]:  # 편도 가능 여부
+            trips = time // (2 * t[i])
+            if time % (2 * t[i]) >= t[i]:
                 trips += 1
             max_transport = trips * w[i]
 
@@ -19,7 +19,7 @@ def solution(a, b, g, s, w, t):
 
         return total_gold >= a and total_silver >= b and total >= a + b
 
-    left, right = 0, int(1e15)  # 충분히 큰 값을 설정
+    left, right = 0, int(1e15)
     answer = right
 
     while left <= right:
@@ -31,3 +31,6 @@ def solution(a, b, g, s, w, t):
             left = mid + 1
 
     return answer
+
+
+print(5 // 2)
